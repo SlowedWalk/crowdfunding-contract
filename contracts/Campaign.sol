6423b1e9cfd5a111ae402b9c058e5e90fac511cb
SlowedWalk <idetoratojo1@gmail.com>
2022-12-11 00:17:11 +0100

    Init: contract and config

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -32,17 +32,47 @@ contract Crowdfunding {
 
         // is everything ok ?
         require (campaign.deadline < block.timestamp, "Deadline must be in the future");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = _amountCollected;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign()  returns () {
-        
+    function donateToCampaign(uint256 _id) public payable  returns () {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if (sent) {
+            campaign.amountCollected += amount;
+        }
     }
 
-    function getDonators()  returns () {
-        
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256 memory) {
+        return campaigns[_id].donators, campaigns[_id].donations;
     }
 
-    function getCampaigns()  returns () {
+    function getCampaigns() public view returns (Campaign memory) {
+        Campaign[] memory AllCampaigns = new Campaign[](numberOfCampaigns);
         
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaign[i];
+
+            AllCampaigns[i] = item;
+        }
+
+        return AllCampaigns;
     }
 }
